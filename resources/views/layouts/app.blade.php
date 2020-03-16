6918d0974e1eccc55df3888584288e605610bf8b
Hîrbu Andrei <andrei.hrb@icloud.com>
2020-03-17 01:06:49 +0200

    bit of css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img style="height: 90px" src="{{ URL::asset('/logo.png') }}" alt="">
+                    <img style="height: 60px" src="{{ URL::asset('/logo.png') }}" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
